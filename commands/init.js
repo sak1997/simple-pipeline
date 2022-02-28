a8479ae2346014d49e3c09810fb7b95a7e95eddb
Ashok Kumar Selvam <aselvam@ncsu.edu>
2022-02-28 17:44:01 -0500

    feat(m1): configure build server

--- commands/init.js
+++ commands/init.js
@@ -1,5 +1,6 @@
 const chalk = require('chalk');
 const path = require('path');
+const M1Helper = require('../lib/m1Helper');
 
 exports.command = 'init';
 exports.desc = 'Prepare tool';
@@ -13,5 +14,9 @@ exports.handler = async argv => {
     const { processor } = argv;
 
     console.log(chalk.green("Preparing computing environment..."));
-   
+    if (processor == 'Arm64') {
+      helper = new M1Helper();
+    }
+
+    await helper.init()
 };
