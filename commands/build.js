f82db9e6705db9ab6c2ee8b7e4c5f39080991d9e
Ashok Kumar Selvam <aselvam@ncsu.edu>
2022-04-07 14:36:24 -0400

    feat: setup for mutation

--- commands/build.js
+++ commands/build.js
@@ -30,19 +30,11 @@ exports.handler = async argv => {
 
     await helper.updateSSHConfig();
 
-    // let jobName = "build";
 
     console.log(chalk.green("started running build job"));
 
     let setupAlreadyDone = false;
     await sshExec("cat status.txt | grep setupCompleted=True > status.txt", helper.sshConfig);
-
-    // await sshExec("cat status.txt", helper.sshConfig).then(function(op) {
-    //   if(op === "setupCompleted=True") {
-    //     console.log("here");
-    //     setupAlreadyDone = true;
-    //   }
-    // });
 
 
     fs.readFile('./status.txt', 'utf8' , (err, data) => {
@@ -58,10 +50,6 @@ exports.handler = async argv => {
       console.log("here!");
     })
 
-    // await execCmd(`rm -r status.txt`);
-
-
-    //let sshCmd = 'ssh -i "/Users/smayanapidugu/Library/Application Support/basicvm/key" -p 22 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o ConnectTimeout=900 ubuntu@192.168.64.74';
     const aptInstallCmd = 'sudo apt-get install -y ';
     const aptUpdateCmd = 'sudo apt-get update';
     let data = YamlParser.parse('./' + buildFile);
@@ -69,11 +57,7 @@ exports.handler = async argv => {
     let setupCmd;
     let runCmd;
     let isAptUpdate = false;
-    // let a = '"'+'mvn -f /home/ubuntu/iTrust2-v10/iTrust2 clean test > /home/ubuntu/mvnOutput.txt'+'"';
-    // await execCmd(`${sshCmd} ${a}`);
-    //setup commands
 
-    // await sshExec("touch setup.sh", helper.sshConfig);
     await execCmd(`echo > setup.sh`);
 
     if(setupAlreadyDone == false) {
@@ -81,32 +65,21 @@ exports.handler = async argv => {
           setupCmd = '';
           if(task.hasOwnProperty("package")){
               if(!isAptUpdate){
-                  // console.log(sshCmd+" "+aptUpdateCmd);
-                  // await sshExec("'echo " + aptUpdateCmd + " >> setup.sh'", helper.sshConfig);
                   await execCmd('echo "' + aptUpdateCmd + '" >> setup.sh');
-                  //await execCmd(`${sshCmd} ${aptUpdateCmd}`);
                   isAptUpdate = true;
               }
             setupCmd = aptInstallCmd + task.package;
           } else{
             setupCmd = task;
           }
-          // console.log(sshCmd +" "+setupCmd);
-          // await sshExec("'echo " + setupCmd + " >> setup.sh'", helper.sshConfig);
           await execCmd('echo "' + setupCmd + '" >> setup.sh');
-        //await execCmd(`${sshCmd} ${setupCmd}`);
       }
 
       await helper.moveToBuildEnv();
 
-      // await execCmd("sed -i 's/\"//g' setup.sh");
-      // await sshExec("cp /bakerx/setup.sh ~/setup.sh", helper.sshConfig);
-      // await sshExec("./setup.sh", helper.sshConfig);
-
       console.log("=====================================================================")
       await sshExec("touch status.txt", helper.sshConfig);
       await sshExec("'echo setupCompleted=True > status.txt'", helper.sshConfig);
-      // await sshExec("echo setupCompleted=True", helper.sshConfig);
     }
 
     // job commands
@@ -115,16 +88,27 @@ exports.handler = async argv => {
             runCmd = '';
             console.log(chalk.green("Executing build job : "+ jobName));
 
-            for (const step of job.steps) {
-                let x = step.run.substring(0, 9);
-                if (x === 'git clone') {
-                  step.run = x + ' https://' + process.env.USER_NAME + ':' + process.env.TOKEN + '@' + step.run.substring(10);
-                }
-                runCmd = '"'+step.run+'"';
-                // console.log(sshCmd+" "+runCmd);
-                await sshExec(runCmd, helper.sshConfig);
-                // await execCmd(`${sshCmd} ${runCmd}`);
+            if (job.mutation) {
+              await mutation(job.mutation);
+            }
+
+            if (job.steps) {
+              for (const step of job.steps) {
+                  let x = step.run.substring(0, 9);
+                  if (x === 'git clone') {
+                    step.run = x + ' https://' + process.env.USER_NAME + ':' + process.env.TOKEN + '@' + step.run.substring(10);
+                  }
+                  runCmd = '"'+step.run+'"';
+                  await sshExec(runCmd, helper.sshConfig);
+              }
             }
         }
     }
+};
+
+async function mutation(info) {
+  console.log(info);
+  let secrets = process.env.USER_NAME + ':' + process.env.TOKEN;
+  let cloneCmd = 'git clone https://' + secrets + '@' + info.url.substring(8);
+  await sshExec(cloneCmd, helper.sshConfig);
 };
