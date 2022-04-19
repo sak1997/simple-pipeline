2b3ba54fad11992c09fc2b2feffcfee44e0fbd03
Ashok Kumar Selvam <aselvam@ncsu.edu>
2022-04-19 12:39:28 -0400

    feat: read n write instance info

--- commands/prod.js
+++ commands/prod.js
@@ -1,6 +1,8 @@
 const chalk = require('chalk');
 const path = require('path');
-
+var fs = require("fs");
+const PropertiesReader = require('properties-reader');
+const instanceFile = "instance.properties"
 
 exports.command = 'prod up';
 exports.desc = 'Create/Destroy production environment';
@@ -14,10 +16,31 @@ exports.handler = async argv => {
     const { processor } = argv;
 
     console.log(chalk.green("Creating production environment..."));
-    let dropletId = createDroplet();
+
+    var fd = fs.openSync(instanceFile, 'a');
+    const properties = PropertiesReader(instanceFile, { writer: { saveSections: true } });
+
+    deleteDroplet(properties.get("ID"));
+    let dropletId = createDroplet(process.env.DIGITAL_OCEAN_TOKEN. process.env.PUB_KEY_PATH);
+    let dropletIp = getDropletIp(dropletId);
+
+    properties.set("ID", dropletId);
+    properties.set("IP", dropletIp);
+    properties.save(instanceFile);
 };
 
 function createDroplet() {
-  console.log("TODO");
-  return 1;
+  console.log("TODO: Create Droplet");
+  return -1;
+}
+
+function getDropletIp(dropletId) {
+  console.log("TODO: Get Droplet ID");
+  return -1;
+}
+
+function deleteDroplet(dropletId) {
+  if ( dropletId !== null ) {
+    console.log("TODO: Delete Droplet");
+  }
 }
