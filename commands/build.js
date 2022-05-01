46246d1ad2334d66dfc14c0baa84b72a6586256c
Ashok Kumar Selvam <aselvam@ncsu.edu>
2022-04-30 21:31:47 -0400

    fix: copy to shared cmd

--- commands/build.js
+++ commands/build.js
@@ -118,8 +118,8 @@ exports.handler = async argv => {
             if (job.steps) {
               for (const step of job.steps) {
                   if (step.shared) {
-                    await sshExec('"sudo mkdir -p '+ outputDirPath + "'", helper.sshConfig);
-                    await sshExec('"sudo cp -r ' + step.shared + " " + outputDirPath + "'", helper.sshConfig)
+                    await sshExec('"sudo mkdir -p '+ outputDirPath + '"', helper.sshConfig);
+                    await sshExec('"sudo cp -r ' + step.shared + " " + outputDirPath + '"', helper.sshConfig)
                   } else if (step.run){
                     let x = step.run.substring(0, 9);
                     if (x === 'git clone') {
